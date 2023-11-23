8d2625d804588103d46d171c40d19f2043b71fbe
Vishal-Agarwal77 <108511250+Vishal-Agarwal77@users.noreply.github.com>
2023-11-23 17:39:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-function getFormvalue() {
+function getFormvalue(e) {
     //Write your code here
-
+	const fname=document.getElementsByName("fname")[0];
+	const lname=document.getElementsByName("lname")[0];
+	alert(`${fname.value} ${lname.value}`)
 }
